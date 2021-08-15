c4645e730fb2a8282428c46c785189f4ac1bd577
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-15 19:18:37 +0530

    debouncing and cancel the api req

--- src/sagas/groups.sagas.ts
+++ src/sagas/groups.sagas.ts
@@ -1,10 +1,12 @@
-import { takeEvery , call , put } from "@redux-saga/core/effects";
+import { call , put , takeLatest , delay} from "@redux-saga/core/effects";
 import { AnyAction } from "redux";
 import { GROUPS_QUERY_CHANGED } from "../actions/action.constant";
 import { groupQureyCompletedAction } from "../actions/groups.actions";
 import {  fetchGroups as fetchGroupsApi } from "../api/groups";
 
 export function* fetchGroups (action : AnyAction) : Generator<any>{
+    yield delay(300);
+
     const groupRes : any = yield call(fetchGroupsApi , {
         query: action.payload,
         status : "all-groups"
@@ -13,6 +15,5 @@ export function* fetchGroups (action : AnyAction) : Generator<any>{
 }
 
 export function* watchGroupQueryChanged(){
-    console.log("watch bala called")
-    yield takeEvery( GROUPS_QUERY_CHANGED, fetchGroups)
+    yield takeLatest(  GROUPS_QUERY_CHANGED, fetchGroups)
 }
