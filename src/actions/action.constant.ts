f72c6c6a64e4e185886eda7b0274d78533158a94
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-16 21:54:40 +0530

    fetch group constants added

--- src/actions/action.constant.ts
+++ src/actions/action.constant.ts
@@ -3,5 +3,7 @@ export const ME_LOGIN = "me/login";
 
 
 
+export const GROUPS_FETCH_ONE  = "groups/fetch_one"
+export const GROUPS_FETCH_ONE_COMPLETED  = "groups/fetch_one_completed"
 export const GROUPS_QUERY_CHANGED = "groups/query_changed"
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed"
