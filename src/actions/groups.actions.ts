5329e0c45dc77faf15a3ae5a2732d87f688a49d0
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-16 21:55:10 +0530

    fetch one group action added

--- src/actions/groups.actions.ts
+++ src/actions/groups.actions.ts
@@ -1,7 +1,12 @@
 
 import { Group } from "../modules/Group"
-import { GROUPS_QUERY_CHANGED , GROUPS_QUERY_COMPLETED } from "./action.constant"
+import { GROUPS_FETCH_ONE, GROUPS_FETCH_ONE_COMPLETED, GROUPS_QUERY_CHANGED , GROUPS_QUERY_COMPLETED } from "./action.constant"
 
 export const groupQureyChangedAction = (query : string) => ({ type :  GROUPS_QUERY_CHANGED , payload : query })
 
 export const groupQureyCompletedAction = (query : string , groups : Group[]) => ({ type : GROUPS_QUERY_COMPLETED , payload : {query , groups}  })
+
+
+export const groupFetchOneAction = (id : number) => ({ type :  GROUPS_FETCH_ONE , payload : id })
+
+export const groupFetchOneCompletedAction = (group: Group) => ({ type :  GROUPS_FETCH_ONE_COMPLETED , payload : group})
