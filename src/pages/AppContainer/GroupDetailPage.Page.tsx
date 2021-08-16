d2a1a1f66f810bb5a9f761695aabf8b0e09df54a
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-16 21:57:40 +0530

    details of group added

--- src/pages/AppContainer/GroupDetailPage.Page.tsx
+++ src/pages/AppContainer/GroupDetailPage.Page.tsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react";
 import {FC, memo} from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
+import { groupFetchOneAction } from "../../actions/groups.actions";
+import { groupByIdSelector } from "../../selectors/groups.selectors";
 interface Props{
 }
 const GroupDetailPage: FC<Props> = (props) =>{
+
     const groupId = +useParams<{groupId : string}>().groupId;
+    const groupById = useSelector(groupByIdSelector);
+
+    const group = groupById[groupId]
+
+    const dispatch = useDispatch()
+   
+    useEffect(()=>{
+        dispatch(groupFetchOneAction(groupId));
+        //eslint-disable-next-line
+    }, [groupId])
+
+    if(!group){
+        return <div>loading the group</div>
+    }
+
 return (
  <>
     <div>
-        these are the deatils of {groupId}
+        these are the deatils of {group.name}
+        id : { group.id}
     </div>
 
     <br />
