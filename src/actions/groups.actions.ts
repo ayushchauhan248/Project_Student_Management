ec250fbb19a7ded639b15e3c325211534e2c9257
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-17 14:10:53 +0530

    fetch one group error action

--- src/actions/groups.actions.ts
+++ src/actions/groups.actions.ts
@@ -1,6 +1,6 @@
 
 import { Group } from "../modules/Group"
-import { GROUPS_FETCH_ONE, GROUPS_FETCH_ONE_COMPLETED, GROUPS_QUERY_CHANGED , GROUPS_QUERY_COMPLETED } from "./action.constant"
+import { GROUPS_FETCH_ONE, GROUPS_FETCH_ONE_COMPLETED, GROUPS_FETCH_ONE_ERROR, GROUPS_QUERY_CHANGED , GROUPS_QUERY_COMPLETED } from "./action.constant"
 
 export const groupQureyChangedAction = (query : string) => ({ type :  GROUPS_QUERY_CHANGED , payload : query })
 
@@ -10,3 +10,5 @@ export const groupQureyCompletedAction = (query : string , groups : Group[]) => 
 export const groupFetchOneAction = (id : number) => ({ type :  GROUPS_FETCH_ONE , payload : id })
 
 export const groupFetchOneCompletedAction = (group: Group) => ({ type :  GROUPS_FETCH_ONE_COMPLETED , payload : group})
+
+export const FetchOneGroupErrorAction = (id: number ,  msg : string ) => ({ type :  GROUPS_FETCH_ONE_ERROR , payload : {id , msg}})
