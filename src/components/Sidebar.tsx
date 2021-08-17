d4fec7339a3681e0c77482181a858c11ad97591e
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-17 14:12:56 +0530

    Sidebar design and add  links

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,48 @@
-import {FC , memo}  from  "react";
-import {logout} from "../api/auth"
-import { meSelector } from "../selectors/auth.selectors";
-import {  useAppSelector } from "../store";
-import Button from "./Button/Button";
+import { FC, memo } from "react";
+import { FaExternalLinkAlt, FaMagento, FaPlay, FaSlideshare } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { logout } from "../api/auth";
 
-interface Props {
-  isVisible: boolean;
+interface Props  {
+    Open:boolean;
 }
-const Sidebar: FC<Props> = ({isVisible}) => {
 
-  const user = useAppSelector(meSelector); 
 
-    return(
-      <div className={`bg-gray-100 w-72 ${(isVisible) ? "" : "hidden"} p-3`}>
-        <div>this is side bar</div>
-        <div className="bg-red-200">{user!.first_name}</div>
-        <Button onClick = {() =>{
-          logout();
-          window.location.href = "/login"
-        }}>Logout</Button>
-      </div>
+const Sidebar: FC<Props> = ({Open}) => {
+    console.log(" Sidebar is rendering");
+
+    return (
+    
+        <div className={Open?" visible relative ":"invisible absolute "}>
+            <div className= "h-screen w-60 bg-gray-300  text-green-500">
+              
+                <div className=" ml-3 mr-3 flex items-center text-3xl font-bold text-gray-800 space-x-4 rounded-md  hover:bg-white py-3">
+                    <FaMagento ></FaMagento>
+                    <Link className="text-xl font-normal " to="/dashboard"> Dashboard</Link>
+                </div>
+                <div className=" ml-3 mr-3 flex items-center text-3xl font-bold text-gray-800 space-x-4 rounded-md hover:bg-white py-3">
+                    <FaPlay></FaPlay>
+                    <Link className=" text-xl font-normal" to="/recordings">Recordings</Link></div>
+                <div className="ml-3 mr-3 flex items-center text-3xl font-bold text-gray-800 space-x-4 rounded-md hover:bg-white py-3">
+                    <FaSlideshare></FaSlideshare>
+                    <Link className="text-xl font-normal " to="/groups"> Groups</Link>
+                </div>
+                <div className=" ml-3 mr-3 flex items-center text-3xl font-bold text-gray-800 space-x-4 rounded-md hover:bg-white py-3">
+                    <FaExternalLinkAlt></FaExternalLinkAlt>
+                    <button className=" text-xl font-normal text-gray-800 border-0" onClick={
+                        () => {
+                            logout();
+                            window.location.href = "/login";
+                        }
+                    }>Logout</button>
+                </div>
+
+
+            </div>
+
+        </div>
     );
 };
-
-Sidebar.defaultProps = {}
-
+Sidebar.defaultProps = {
+}
 export default memo(Sidebar);
