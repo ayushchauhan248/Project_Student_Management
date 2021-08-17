1c16bcb76ba7a37c8ff24d27356efc6bf1a770db
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-17 14:10:28 +0530

    added fetch one error constant

--- src/actions/action.constant.ts
+++ src/actions/action.constant.ts
@@ -5,5 +5,6 @@ export const ME_LOGIN = "me/login";
 
 export const GROUPS_FETCH_ONE  = "groups/fetch_one"
 export const GROUPS_FETCH_ONE_COMPLETED  = "groups/fetch_one_completed"
+export const GROUPS_FETCH_ONE_ERROR  = "groups/fetch_one_error"
 export const GROUPS_QUERY_CHANGED = "groups/query_changed"
 export const GROUPS_QUERY_COMPLETED = "groups/query_completed"
