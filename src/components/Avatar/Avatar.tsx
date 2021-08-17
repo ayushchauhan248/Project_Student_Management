ef397fca4a60d7380a6929549bbe02453dac5d17
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-17 14:13:18 +0530

    avatar props changed for using in groups

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,19 +1,26 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import {ButtonHTMLAttributes, FC, memo} from "react";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-    theme : 'active' | 'inactive';
+    theme ?: 'active' | 'inactive';
+    imageUrl: string;
 }
-const Avatar: FC<Props> = ({ theme, className}) =>{
+const Avatar: FC<Props> = ({ theme,imageUrl, className}) =>{
     const themeClasses =
         theme === "inactive" ? "bg-red-600" : "bg-green-500";
 
 return (
     <div className="relative inline-block">
-        <img className="inline-block object-cover w-16 h-16 rounded-full" src="https://images.pexels.com/photos/2955305/pexels-photo-2955305.jpeg?auto=compress&cs=tinysrgb&h=650&w=940" alt="Profile image"/>
-        <span className={"absolute bottom-0 right-0 inline-block w-3 h-3  border-2 border-white rounded-full "+ themeClasses +" "+ className}></span>
+        <img className="inline-block object-cover w-16 h-16 rounded-full" 
+        src={imageUrl} 
+        onError={(event: any) => { event.target.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTo8ot81o5G-DZLv5GatY5S5Jsc2rHobmXkQ&usqp=CAU"; }} 
+        alt="Profile image"/>
+        <span className={"absolute bottom-4 right-0 inline-block w-4 h-4  border-2 border-white rounded-full "+ themeClasses +" "+ className}></span>
   </div>
 );
 };
 Avatar.defaultProps={
+    theme:"active",
+    imageUrl:"https://www.w3schools.com/howto/img_avatar2.png"
 }
-export default memo(Avatar);
+export default memo(Avatar)
