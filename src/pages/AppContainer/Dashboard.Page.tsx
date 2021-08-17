dc7cd9117a7d8c0b0ca38d79e747256260c74533
ayushchauhan248 <ayush.chauhan_cs19@gla.ac.in>
2021-08-17 14:14:36 +0530

    make dashboard empty

--- src/pages/AppContainer/Dashboard.Page.tsx
+++ src/pages/AppContainer/Dashboard.Page.tsx
@@ -1,48 +1,18 @@
-
-import {FC , memo}  from  "react";
-import { FaSpinner } from "react-icons/fa";
-import { useDispatch } from "react-redux";
-import { Link} from "react-router-dom";
-import { groupQureyChangedAction } from "../../actions/groups.actions";
-import { groupsLoadingSelector, groupQuerySelector, groupsSelector } from "../../selectors/groups.selectors";
+import React from "react";
+import { meSelector } from "../../selectors/auth.selectors";
 import { useAppSelector } from "../../store";
 
-interface Props {}
-
-const Dashboard: FC<Props> = () => {
-
-    const query = useAppSelector(groupQuerySelector);
-
-    const Loading = useAppSelector(groupsLoadingSelector)
-
-    const groups = useAppSelector(groupsSelector);
-    
-    const dispatch = useDispatch();
-
-    return(
-        <div>
-            <h1>this is Dashboard page</h1>
-            <Link to="/recordings"><span className = "text-blue-500" >Go to recording</span></Link>
-            <br></br>
-            <input type="text" 
-            value={query} 
-            onChange={(e)=>
-            {
-                dispatch(groupQureyChangedAction(e.target.value))
-            }}/>
-            {Loading &&  <FaSpinner className="animate-spin m-3"></FaSpinner> }
-            <div>
-                {
-                    groups.map((group) => (
-                        <div key= { group.id }>  <Link to={"/groups/" + group.id}>{group.name}</Link></div>
-                    ))
-                }
-                {!Loading && groups.length === 0 &&  " NO DATA FOUND "}
-            </div>
+interface Props{}
+const Dashboard: React.FC<Props> = (props) => {
+    const user = useAppSelector(meSelector);
+    return (
+        <div>This is dashboard page
+           
+           <div className=" text-yellow-400">{user!.first_name}</div>
+            
         </div>
     );
 };
-
-Dashboard.defaultProps = {}
-
-export default memo(Dashboard);
+Dashboard.defaultProps = {
+}
+export default Dashboard;
